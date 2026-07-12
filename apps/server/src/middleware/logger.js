f29205825e5c832d7ce5757ba887b4a91eecc24d
AzariaSW <azariaandabenet@gmail.com>
2026-07-12 22:40:44 +0300

    refactor: consolidate HTTP request logging middleware

--- apps/server/src/middleware/logger.js
+++ apps/server/src/middleware/logger.js
@@ -1,23 +1,30 @@
 import morgan from "morgan";
 import logger from "../logger/logger.js";
 
-
 const stream = {
-
-    write(message){
-
-        logger.info(
-            message.trim()
-        );
-
+    write(message) {
+        logger.info(message.trim());
     }
-
 };
 
+const httpLogger = morgan((tokens, req, res) => {
 
-export default morgan(
-    "combined",
-    {
-        stream
-    }
-);
+    return [
+
+        `[${req.id ?? "-"}]`,
+
+        tokens.method(req, res),
+
+        tokens.url(req, res),
+
+        tokens.status(req, res),
+
+        `${tokens["response-time"](req, res)} ms`
+
+    ].join(" ");
+
+}, {
+    stream
+});
+
+export default httpLogger;
--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,7 +1,12 @@
 import "dotenv/config";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
 
 import express from "express";
 
+import requestId from "./middleware/requestId.js";
+import httpLogger from "./middleware/logger.js";
 import apiRoutes from "./routes/index.js";
 
 import {
@@ -19,11 +24,17 @@ process.env.PORT || 5000;
 
 
 
-app.use(
-    express.json()
-);
+app.use(express.json());
 
+app.use(helmet());
 
+app.use(cors());
+
+app.use(compression());
+
+app.use(requestId);
+
+app.use(httpLogger);
 
 app.use(
     "/api",
