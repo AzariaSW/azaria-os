b6b3f9ebe323018d6559e1eff75326dd22cc39a4
AzariaSW <azariaandabenet@gmail.com>
2026-07-23 23:54:18 +0300

    style(client): add global CSS reset and one sample page

--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import AppProviders from "./providers/AppProviders";
+import router from "./routes/router";
+import "./styles/globals.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AppProviders>
+      <RouterProvider router={router} />
+    </AppProviders>
   </StrictMode>,
-)
+);
