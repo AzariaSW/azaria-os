ab67be658014254df4dc1ca6d316c14b58d474de
AzariaSW <azariaandabenet@gmail.com>
2026-07-24 22:41:56 +0300

    feat(client): configure React Query provider

--- apps/client/src/providers/AppProviders.jsx
+++ apps/client/src/providers/AppProviders.jsx
@@ -1,10 +1,15 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+import queryClient from "../lib/queryClient";
 
 function AppProviders({ children }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <>
+    <QueryClientProvider client={queryClient}>
+      {children} <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    </>
   );
 }
 
