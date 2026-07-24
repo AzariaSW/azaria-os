faf39d791d985d34ff0d969f815546cd4ea32e26
AzariaSW <azariaandabenet@gmail.com>
2026-07-25 00:32:53 +0300

    feat(client): configure toast

--- apps/client/src/providers/AppProviders.jsx
+++ apps/client/src/providers/AppProviders.jsx
@@ -2,13 +2,16 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import queryClient from "../lib/queryClient";
+import ToastProvider from "./ToastProvider";
 
 function AppProviders({ children }) {
   return (
     <>
-    <QueryClientProvider client={queryClient}>
-      {children} <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children} <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+
+      <ToastProvider />
     </>
   );
 }
